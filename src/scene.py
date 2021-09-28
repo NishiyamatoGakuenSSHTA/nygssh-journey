dc8ceb7c4d33dda5d57439d8db4250d384161f83
hanebarla <barla.wings@gmail.com>
2021-09-28 16:58:45 +0900

    add scene data

--- src/scene.py
+++ src/scene.py
@@ -1,15 +1,26 @@
 import numpy as np
+import os
 
 
 GameSize = (250, 720)
 
 
+class MapScene():
+    def __init__(self, SceneCreateInfoFile) -> None:
+        pass
+
 class SceneManeger():
-    def __init__(self):
-        self.scenekind = 0 # 0: idle, 1: battle
+    def __init__(self, SceneDataDir="./data"):
+        datafiles = os.listdir(SceneDataDir)
+        self.jsonfiles = [f for f in datafiles if ".json" in f]
+
+        self.scenekind = 0 # 0: map, 1: battle
         self.backgrond = {
                             0: np.zeros(GameSize, dtype=np.uint8),
                             1: np.full(GameSize, 50, dtype=np.uint8)
                          }
+
+    def initialize():
+        pass
 
 
